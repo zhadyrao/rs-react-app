441b4ab69f405f7735d6aef261b8d0efdca089c6
Zhadyra Oralkhanova <oralkhanova.zhadyra@gmail.com>
2025-07-21 02:50:09 +0500

    chore: test pre-push

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -37,7 +37,7 @@ class SearchBar extends Component<Props, State> {
           role="textbox"
         />
         <button type="submit" className="bg-red-500 text-white px-4 py-2">
-          Search
+          Search item
         </button>
       </form>
     );
