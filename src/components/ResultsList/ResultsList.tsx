6c8696b3f9995ca0e4cf89b1053e6acb4f9e83d3
Zhadyra Oralkhanova <oralkhanova.zhadyra@gmail.com>
2025-07-21 02:47:18 +0500

    chore: test pre-push

--- src/components/ResultsList/ResultsList.tsx
+++ src/components/ResultsList/ResultsList.tsx
@@ -11,7 +11,7 @@ class ResultsList extends Component<Props> {
     const { starships } = this.props;
 
     if (!starships || starships.length === 0) {
-      return <p className="text-gray-500">No results found.</p>;
+      return <p className="text-gray-500">No results found</p>;
     }
 
     return (
